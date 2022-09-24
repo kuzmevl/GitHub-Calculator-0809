090ebd8cc6fd6e762d750ba429ec864d399ec167
kuzmevl <kuzmenko055@gmail.com>
2022-09-25 00:43:42 +0300

    variables declared out of main

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,18 +2,30 @@ package app;
 
 public class Main {
 
+    static int a;
+
+    static int b;
+
+    static int c;
+
+    static int d;
+
+    static int e;
+
+    static double f;
+
     public static void main(String[] args) {
         System.out.println("Calculator for base calculations");
         System.out.println("Version 1.0");
-        int a = 8;
-        int b = 2;
-        int c = add(a, b);
+        a = 8;
+        b = 2;
+        c = add(a, b);
         System.out.println(c);
-        int d = multiply(a, b);
+        d = multiply(a, b);
         System.out.println(d);
-        int e = subtract(a, b);
+        e = subtract(a, b);
         System.out.println(e);
-        double f = divide (a, b);
+        f = divide(a, b);
         System.out.println(f);
     }
 
